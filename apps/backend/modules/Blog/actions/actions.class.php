fcb772b5ba47d8f3301b1867e744c32427f89527
root <root@nya.(none)>
2010-11-15 00:12:32 +0300

    Add blog backend

---
+++ apps/backend/modules/Blog/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/BlogGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/BlogGeneratorHelper.class.php';
+
+/**
+ * Blog actions.
+ *
+ * @package    Empaty
+ * @subpackage Blog
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BlogActions extends autoBlogActions
+{
+}
---
+++ apps/backend/modules/Blog/lib/BlogGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Blog module helper.
+ *
+ * @package    Empaty
+ * @subpackage Blog
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BlogGeneratorHelper extends BaseBlogGeneratorHelper
+{
+}
