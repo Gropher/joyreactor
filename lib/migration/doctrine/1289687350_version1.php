e91ec73a668fd65525ba3ca31dfaf09ea6224081
root <root@nya.(none)>
2010-11-15 00:57:39 +0300

    bad migration fix... to late =(

--- lib/migration/doctrine/1289687350_version1.php
+++ lib/migration/doctrine/1289687350_version1.php
@@ -6,7 +6,6 @@ class Version1 extends Doctrine_Migration_Base
 {
     public function up()
     {
-        $this->dropTable('sf_guard_user');
         $this->addColumn('blog', 'best', 'integer', '1', array(
              'notnull' => '1',
              'default' => '0',
@@ -21,86 +20,6 @@ class Version1 extends Doctrine_Migration_Base
 
     public function down()
     {
-        $this->createTable('sf_guard_user', array(
-             'id' => 
-             array(
-              'type' => 'integer',
-              'primary' => '1',
-              'autoincrement' => '1',
-              'length' => '4',
-             ),
-             'username' => 
-             array(
-              'type' => 'string',
-              'notnull' => '1',
-              'unique' => '1',
-              'length' => '128',
-             ),
-             'algorithm' => 
-             array(
-              'type' => 'string',
-              'default' => 'sha1',
-              'notnull' => '1',
-              'length' => '128',
-             ),
-             'salt' => 
-             array(
-              'type' => 'string',
-              'length' => '128',
-             ),
-             'password' => 
-             array(
-              'type' => 'string',
-              'length' => '128',
-             ),
-             'is_active' => 
-             array(
-              'type' => 'boolean',
-              'default' => '1',
-              'length' => '25',
-             ),
-             'is_super_admin' => 
-             array(
-              'type' => 'boolean',
-              'default' => '0',
-              'length' => '25',
-             ),
-             'last_login' => 
-             array(
-              'type' => 'timestamp',
-              'length' => '25',
-             ),
-             'created_at' => 
-             array(
-              'notnull' => '1',
-              'type' => 'timestamp',
-              'length' => '25',
-             ),
-             'updated_at' => 
-             array(
-              'notnull' => '1',
-              'type' => 'timestamp',
-              'length' => '25',
-             ),
-             ), array(
-             'type' => 'MYISAM',
-             'indexes' => 
-             array(
-              'is_active_idx' => 
-              array(
-              'fields' => 
-              array(
-               0 => 'is_active',
-              ),
-              ),
-             ),
-             'primary' => 
-             array(
-              0 => 'id',
-             ),
-             'collate' => 'utf8_unicode_ci',
-             'charset' => 'utf8',
-             ));
         $this->removeColumn('blog', 'best');
         $this->removeColumn('blog', 'count');
         $this->removeColumn('blog', 'description');
