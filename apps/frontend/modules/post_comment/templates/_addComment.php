b2b424283144cacb24561bbcc1e2507865ee3b37
koka <miptpatriot@gmail.com>
2010-11-19 18:28:40 +0300

    перенёс яваскрипт в отдельный .js

--- apps/frontend/modules/post_comment/templates/_addComment.php
+++ apps/frontend/modules/post_comment/templates/_addComment.php
@@ -4,8 +4,10 @@
                 array('id' => 'add_comment_form'.$post->getId()."_".$parent,
                   'method' => 'post',
                   'onkeypress' => "ctrlEnter(event, this);",
-                  'onsubmit' => "return AIM.submit(this, {'onStart' : startAddComment".$post->getId()."_".$parent.",
-                  'onComplete' : completeAddComment".$post->getId()."_".$parent."})") ) ?>
+                  'onsubmit' => "return AIM.submit(this,
+                    {'onStart' : function() { startAddComment('".$post->getId()."_".$parent."'); },
+                     'onComplete' : function(response) { completeAddComment('".$post->getId()."_".$parent."', response); }
+                    })") ) ?>
     <? if($parent): ?>
         <? echo input_hidden_tag('parent_id', $parent->getId()) ?>
     <? endif ?>
@@ -15,24 +17,3 @@
     <a href="#" onclick="$j('#comment_picture_url', $j(this).parent()).show(); $j(this).hide(); return false;">Вставить картинку</a>
     <? echo input_tag('comment_picture_url', null, array('style' => 'display:none')) ?>
 </form>
-<script type="text/javascript">
-    function startAddComment<? echo $post->getId()."_".$parent ?>(response)
-    {
-        $j('.submit_add_comment<? echo $post->getId()."_".$parent ?>').attr("disabled", true);
-    }
-
-    function completeAddComment<? echo $post->getId()."_".$parent ?>(response)
-    {
-        $j('#add_comment_form<? echo $post->getId()."_".$parent ?>').keypress(function(e){ctrlEnter(e, this)});
-        $j('.addcomment').hide("fast");
-        $j('.addcommentInline').hide("fast");
-        $j('#comment_list<? echo $post->getId()."_".$parent ?>').html(response);
-        $j('.submit_add_comment<? echo $post->getId()."_".$parent ?>').removeAttr("disabled");
-        $j('#add_comment_form<? echo $post->getId()."_".$parent ?> :input[type=textarea]').each(function(){
-          $j(this).val('');
-        });
-        $j('#add_comment_form<? echo $post->getId()."_".$parent ?> input#comment_picture_url').each(function(){
-          $j(this).val('');
-        });
-    }
-</script>
--- web/js/comments.js
+++ web/js/comments.js
@@ -12,3 +12,23 @@ function UpdateCommentsCss(){
     }
   );
 }
+
+function startAddComment(formId)
+{
+    $j('.submit_add_comment' + formId).attr("disabled", true);
+}
+
+function completeAddComment(formId, response)
+{
+    $j('#add_comment_form' + formId).keypress(function(e){ctrlEnter(e, this)});
+    $j('.addcomment').hide("fast");
+    $j('.addcommentInline').hide("fast");
+    $j('#comment_list' + formId).html(response);
+    $j('.submit_add_comment' + formId).removeAttr("disabled");
+    $j('#add_comment_form' + formId + ' :input[type=textarea]').each(function(){
+      $j(this).val('');
+    });
+    $j('#add_comment_form' + formId + ' input#comment_picture_url').each(function(){
+      $j(this).val('');
+    });
+}
