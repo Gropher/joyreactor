5596a75523c6fd373e8cdaf63061292d064d99cc
koka <miptpatriot@gmail.com>
2010-11-19 12:53:53 +0300

    коммент без текста с картинкой теперь тоже постится.

--- apps/frontend/modules/post_comment/actions/actions.class.php
+++ apps/frontend/modules/post_comment/actions/actions.class.php
@@ -25,46 +25,8 @@ class post_commentActions extends sfActions {
     public function executeCreate(sfWebRequest $request) {
         $this->post = Doctrine::getTable('Post')->find(array($request->getParameter('post_id')));
         $this->parent = Doctrine::getTable('PostComment')->find(array($request->getParameter('parent_id')));
-        $user = $this->getUser()->getGuardUser();
-        if($this->getUser()->isAuthenticated() && $this->post && $request->getParameter('comment_text') !== null && trim($request->getParameter('comment_text')) !== '') {
-            $text = $request->getParameter('comment_text');
-            if($request->isMethod("GET"))
-              $text = urldecode($text);
-
-            if($this->getRequestParameter('comment_picture_url')) {
-                $filename = pathinfo($this->getRequestParameter('comment_picture_url'));
-                $extension = $filename["extension"] ? $filename["extension"] : "jpg";
-                $filename = time().rand(1, 999999).".".$extension;
-                $thumbnail = new sfThumbnail(811, 0, true, false, 100, sfConfig::get('app_sfThumbnailPlugin_adapter','sfGDAdapter'));
-                $thumbnail->loadFile($this->getRequestParameter('comment_picture_url'));
-                $thumbnail->save(sfConfig::get('sf_upload_dir').'/'.$filename);
-                $text .= "<br/><img src='/uploads/" . $filename . "' />";
-            }
-            
-            $comment = new PostComment();
-            $comment->setUser($user);
-            $comment->setPost($this->post);
-            if($this->parent)
-                $comment->setParent($this->parent);
-            $comment->setComment($text);
-            $comment->save();
-//            try {
-//                if($this->parent)
-//                    nahoMail::send('Ответ',
-//                        nahoMail::getBody('partial', 'post_comment/notify_comment',
-//                        array('post' => $this->post, 'parent' => $this->parent, 'comment' => $comment)),
-//                        $this->parent->getUser()->getProfile()->getEMail());
-//                else
-//                    nahoMail::send('Комментарий',
-//                        nahoMail::getBody('partial', 'post_comment/notify_comment',
-//                        array('post' => $this->post, 'parent' => $this->parent, 'comment' => $comment)),
-//                        $this->post->getUser()->getProfile()->getEMail());
-//            }catch(Exception $e){}
-            $this->curUser = $this->getUser()->getGuardUser();
-            if($this->curUser) {
-                Cookie::setCookie($this->curUser, "comments".$this->post->getId(), $this->post->getAllComments('count'), time() + 24 * 60 * 60);
-                Cookie::setCookie($this->curUser, "comments".$this->post->getId()."Time", date("Y-m-d H:i:s"), time() + 24 * 60 * 60);
-            }
+        if($this->getUser()->isAuthenticated() && $this->post) {
+          $this->AddComment($request);
         }
         sfApplicationConfiguration::getActive()->loadHelpers('I18N');
         if(!$this->getUser()->isAuthenticated()){
@@ -84,5 +46,56 @@ class post_commentActions extends sfActions {
             return $this->renderPartial('post_comment/commentList', array('comments' => $this->parent->getComments()));
         else
             return $this->renderPartial('post_comment/commentList', array('comments' => $this->post->getComments()));
+    }
+
+    private function AddComment(sfWebRequest $request)
+    {
+        $text = $request->getParameter('comment_text');
+        if($request->isMethod("GET"))
+          $text = urldecode($text);
+
+        if($this->getRequestParameter('comment_picture_url')) {
+            $filename = pathinfo($this->getRequestParameter('comment_picture_url'));
+            $extension = $filename["extension"] ? $filename["extension"] : "jpg";
+            $filename = time().rand(1, 999999).".".$extension;
+            $thumbnail = new sfThumbnail(811, 0, true, false, 100, sfConfig::get('app_sfThumbnailPlugin_adapter','sfGDAdapter'));
+            $thumbnail->loadFile($this->getRequestParameter('comment_picture_url'));
+            $thumbnail->save(sfConfig::get('sf_upload_dir').'/'.$filename);
+
+            if($text)
+              $text .= "<br/>";
+            $text .= "<img src='/uploads/" . $filename . "' />";
+        }
+
+        if(!trim($text))
+        {
+          return;
+        }
+
+        $user = $this->getUser()->getGuardUser();
+        $comment = new PostComment();
+        $comment->setUser($user);
+        $comment->setPost($this->post);
+        if($this->parent)
+            $comment->setParent($this->parent);
+        $comment->setComment($text);
+        $comment->save();
+//            try {
+//                if($this->parent)
+//                    nahoMail::send('Ответ',
+//                        nahoMail::getBody('partial', 'post_comment/notify_comment',
+//                        array('post' => $this->post, 'parent' => $this->parent, 'comment' => $comment)),
+//                        $this->parent->getUser()->getProfile()->getEMail());
+//                else
+//                    nahoMail::send('Комментарий',
+//                        nahoMail::getBody('partial', 'post_comment/notify_comment',
+//                        array('post' => $this->post, 'parent' => $this->parent, 'comment' => $comment)),
+//                        $this->post->getUser()->getProfile()->getEMail());
+//            }catch(Exception $e){}
+        $this->curUser = $this->getUser()->getGuardUser();
+        if($this->curUser) {
+            Cookie::setCookie($this->curUser, "comments".$this->post->getId(), $this->post->getAllComments('count'), time() + 24 * 60 * 60);
+            Cookie::setCookie($this->curUser, "comments".$this->post->getId()."Time", date("Y-m-d H:i:s"), time() + 24 * 60 * 60);
+        }
     }
 }
