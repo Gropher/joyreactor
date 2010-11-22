b8b419c98633a04f82dbd52ea932d003ee83a41f
koka <miptpatriot@gmail.com>
2010-11-22 22:42:42 +0300

    #117

--- apps/frontend/templates/_blogLeftMenu.php
+++ apps/frontend/templates/_blogLeftMenu.php
@@ -2,6 +2,11 @@
     <li>
         <h2><? echo __('Тег').' "'.$blog->getName().'"'?></h2>
         <div class="sidebarContent">
+            <?php if($blog->getAvatar()) : ?>
+              <img src="<?php echo $blog->getAvatar(); ?>" alt="<?php echo $blog->getName(); ?>" />
+           <?php else : ?>
+             <img src="/images/default_avatar.jpeg" alt="<?php echo $blog->getName(); ?>" />
+            <?php endif ?>
             <p><? echo __('Рейтинг:').' '.$blog->getRating()?></p>
             <p><? echo __('Постов:').' '.$blog->getLine('count')?></p>
             <? include_partial('favorite/blog_link', array('blog' => $blog)) ?>
