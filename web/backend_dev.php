aa81cc40906187c090b4e0137d923f3eade1bca1
root <root@joyreactor.(none)>
2011-02-01 22:06:17 +0100

    fix backend cache

--- web/backend_dev.php
+++ web/backend_dev.php
@@ -10,4 +10,14 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '89.222.254.19
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'dev', true);
+
+
+//enable Doctrine cache
+$manager = Doctrine_Manager::getInstance();
+$servers = array('host' => 'localhost', 'port' => 11211, 'persistent' => true);
+//$cacheDriver = new Doctrine_Cache_Memcache(array('servers' => $servers, 'compression' => false));
+$cacheDriver = new Doctrine_Cache_Apc();
+$manager->setAttribute(Doctrine::ATTR_QUERY_CACHE, $cacheDriver);
+$manager->setAttribute(Doctrine::ATTR_RESULT_CACHE, $cacheDriver);
+
 sfContext::createInstance($configuration)->dispatch();
