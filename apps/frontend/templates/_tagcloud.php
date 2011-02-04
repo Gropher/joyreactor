df4b2283d8b0ad818921ca9c95382aa4d935f9ab
koka <miptpatriot@gmail.com>
2011-02-04 22:40:13 +0300

    #154 3) Поставить в заголовки h2 теги из облака тегов

--- apps/frontend/templates/_tagcloud.php
+++ apps/frontend/templates/_tagcloud.php
@@ -4,12 +4,12 @@
         <div id="tagcloud" style="width:243px;">
           <?php $tags = Blog::getTags(); ?>
           <?php foreach($tags['tags'] as $tag): ?>
-            <a href="<?php echo url_for('@tag?name=' . $tag['tag']);?>" class="tagcloudlink" style="font-size: <?php
+          <h2><a href="<?php echo url_for('@tag?name=' . $tag['tag']);?>" class="tagcloudlink" style="font-size: <?php
                 // формула из jquery.tagcloud
                 echo 1 + 2 * ($tag['count'] - $tags['min']) / ($tags['max'] - $tags['min']);
               ?>em;" >
               <?php echo $tag['tag']; ?>
-            </a>
+            </a></h2>
           <?php endforeach ?>
         </div>
         <?php echo link_to('Все тэги', '@list-tags'); ?>
